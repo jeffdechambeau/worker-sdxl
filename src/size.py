cde2d881dd071d42ad3326989cc6e6958839cb13
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-10 14:37:01 -0500

    Create size.py

---
+++ src/size.py
@@ -0,0 +1,23 @@
+size_config = {
+    "small": {
+        "3:2": {"height": 340, "width": 512},
+        "5:4": {"height": 408, "width": 512},
+        "1:1": {"height": 512, "width": 512},
+        "4:5": {"height": 512, "width": 408},
+        "2:3": {"height": 512, "width": 340}
+    },
+    "medium": {
+        "3:2": {"height": 512, "width": 768},
+        "5:4": {"height": 616, "width": 768},
+        "1:1": {"height": 768, "width": 768},
+        "4:5": {"height": 768, "width": 616},
+        "2:3": {"height": 768, "width": 512}
+    },
+    "large": {
+        "3:2": {"height": 680, "width": 1024},
+        "5:4": {"height": 820, "width": 1024},
+        "1:1": {"height": 1024, "width": 1024},
+        "4:5": {"height": 1024, "width": 820},
+        "2:3": {"height": 1024, "width": 680}
+    }
+}
