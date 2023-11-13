46ac0ec68012ff4264f7d52d700db8339a410da3
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-13 11:49:55 -0500

    changing approach

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -1,53 +1,98 @@
-import os
-import torch
+import time
 
-from diffusers.utils import load_image
 import runpod
+import requests
+from requests.adapters import HTTPAdapter, Retry
 
-from runpod.serverless.utils.rp_validator import validate
-from rp_schemas import INPUT_SCHEMA
-
-from handler.helpers import _save_and_upload_images
-from handler.generate import generate_with_model
-
-torch.cuda.empty_cache()
+automatic_session = requests.Session()
+retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
+automatic_session.mount('http://', HTTPAdapter(max_retries=retries))
 
 
-def process_input(job):
-    validated_input = validate(job["input"], INPUT_SCHEMA)
-    if 'errors' in validated_input:
-        return None, {"error": validated_input['errors']}
-    print(validated_input)
+# ---------------------------------------------------------------------------- #
+#                              Automatic Functions                             #
+# ---------------------------------------------------------------------------- #
 
-    job_input = validated_input['validated_input']
-    job_input['seed'] = job_input.get(
-        'seed') or int.from_bytes(os.urandom(2), "big")
-    print(job_input)
-    return job_input, None
+def wait_for_service(url):
+    '''
+    Check if the service is ready to receive requests.
+    '''
+    while True:
+        try:
+            requests.get(url)
+            return
+        except requests.exceptions.RequestException:
+            print("Service not ready yet. Retrying...")
+        except Exception as err:
+            print("Error: ", err)
+
+        time.sleep(0.2)
 
 
-@torch.inference_mode()
-def generate_image(job):
-    job_input, error = process_input(job)
+def run_inference(params):
+    config = {
+        "baseurl": "http://127.0.0.1:3000",
+        "api": {
+            "txt2img":  ("POST", "/sdapi/v1/txt2img"),
+            "img2img":  ("POST", "/sdapi/v1/img2img"),
+            "png-info": ("POST", "/sdapi/v1/png-info"),
+            "getModels": ("GET", "/sdapi/v1/sd-models"),
+            "getOptions": ("GET", "/sdapi/v1/options"),
+            "setOptions": ("POST", "/sdapi/v1/options"),
 
-    if error:
-        return error
-
-    generator = torch.Generator("cuda").manual_seed(job_input['seed'])
-    init_image = load_image(job_input['image_url']).convert(
-        "RGB") if job_input['image_url'] else None
-
-    output = generate_with_model(job_input, generator, init_image)
-    image_urls = _save_and_upload_images(output, job['id'])
-    print('output', output)
-    print('image_urls', image_urls)
-
-    return {
-        "images": image_urls,
-        "image_url": image_urls[0],
-        "seed": job_input['seed'],
-        "refresh_worker": bool(init_image)
+            # https://github.com/Mikubill/sd-webui-controlnet/wiki/API#web-api
+            "getControlNetModels": ("GET", "/controlnet/model_list"),
+            "getControlNetModules": ("GET", "/controlnet/module_list"),
+            "getControlNetAPIVersion": ("GET", "/controlnet/version"),
+            "getControlNetDetect": ("POST", "/controlnet/detect"),
+        },
+        "timeout": 600
     }
 
+    api_name = params["api_name"]
+    path = None
 
-runpod.serverless.start({"handler": generate_image})
+    if api_name in config["api"]:
+        api_config = config["api"][api_name]
+    else:
+        raise Exception("Method '%s' not yet implemented" % api_name)
+
+    api_verb = api_config[0]
+    api_path = api_config[1]
+
+    response = {}
+
+    if api_verb == "GET":
+        response = automatic_session.get(
+            url='%s%s' % (config["baseurl"], api_path),
+            timeout=config["timeout"])
+
+    if api_verb == "POST":
+        response = automatic_session.post(
+            url='%s%s' % (config["baseurl"], api_path),
+                json=params,
+            timeout=config["timeout"])
+
+    return response.json()
+
+
+# ---------------------------------------------------------------------------- #
+#                                RunPod Handler                                #
+# ---------------------------------------------------------------------------- #
+def handler(event):
+    '''
+    This is the handler function that will be called by the serverless.
+    '''
+
+    json = run_inference(event["input"])
+
+    # return the output that you want to be returned like pre-signed URLs to output artifacts
+    return json
+
+
+if __name__ == "__main__":
+    wait_for_service(url='http://127.0.0.1:3000/sdapi/v1/txt2img')
+
+    print("WebUI API Service is ready. Starting RunPod...")
+
+    runpod.serverless.start({"handler": handler})
