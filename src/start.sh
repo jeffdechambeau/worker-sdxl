659f09541b75876da39b7ceb743ef24c2448db89
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-20 13:25:22 -0500

    changed python ref

--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,7 @@
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
+python3 stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
 
 echo "Starting RunPod Handler"
-python /rp_handler.py
+python3 -u /rp_handler.py
