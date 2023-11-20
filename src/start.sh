1d621c6a8163dc199fac80a01dbc6664b7aad743
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-20 13:05:29 -0500

    removed -u

--- src/start.sh
+++ src/start.sh
@@ -6,4 +6,4 @@ echo "Starting WebUI API"
 python stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
 
 echo "Starting RunPod Handler"
-python -u /rp_handler.py
+python /rp_handler.py
