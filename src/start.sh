f5860978353ca64088a6d5e6d633b693938d3c58
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-20 12:24:26 -0500

    changed start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-echo "Entering /start.sh"
-
-# For some reason, pod cloud env doesn't look for packages here, which is where runpod module is
-# so the first addition is to make the env in cloud aware of local dist-packages
-# the second addition is to make container aware of where modeules were installed on network volume
-export PYTHONPATH="$PYTHONPATH:/usr/local/lib/python3.10/dist-packages:/workspace/venv/lib/python3.10/site-packages"
-
-python3 --version
-python3 -c "import sys; print(sys.path)"
-pip --version
-pip freeze
-
-#list files in current folder
-pwd
-ls
+echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python3 /workspace/sd/stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --opt-sdp-attention --port 3000 --api --nowebui --skip-version-check
+python stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
+
 echo "Starting RunPod Handler"
-python3 -u ./rp_handler.py
+python -u /rp_handler.py
