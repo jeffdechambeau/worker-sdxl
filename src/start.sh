1367e762d386555f9b3bef71cd539e25db074e97
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-21 18:09:42 -0500

    finally have path right

--- src/start.sh
+++ src/start.sh
@@ -9,7 +9,7 @@ ls /workspace
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python3 /workspace/stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
+python3 /workspace/sd/stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
 
 echo "Starting RunPod Handler"
 python3 -u /rp_handler.py
