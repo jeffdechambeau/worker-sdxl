dfdff38b3e7b41cb1b6c4f5efdc55cd8a435bd77
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-22 10:50:49 -0500

    changed launch cmd

--- src/start.sh
+++ src/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-echo "Worker Initiated"
-
 echo "Starting WebUI API"
-python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt /model.safetensors --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
+python3 /workspace/sd/stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
 
 echo "Starting RunPod Handler"
-python -u /rp_handler.py
+python3 -u /rp_handler.py
