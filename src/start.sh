504422db69e9a27bfa2dfd1083989b264b99dba4
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-11-21 19:53:18 -0500

    yet another dependency!

--- src/start.sh
+++ src/start.sh
@@ -4,6 +4,7 @@ echo "Worker Initiated"
 pip install -U pip
 pip install -U httpcore
 pip install -U open-clip-torch
+pip install -U rich
 
 echo "Starting WebUI API"
 python3 /workspace/sd/stable-diffusion-webui/webui.py --api --no-half --disable-nan-check 
