65a0234d4568ffc3c2214aa864e8a6720e5c5a31
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-12-02 17:13:09 -0500

    updating refs

--- src/start.sh
+++ src/start.sh
@@ -3,7 +3,7 @@
 echo "Worker Initiated"
 
 echo "Starting WebUI API"
-python3.10 /workspace/sd/stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --port 3000 --api --nowebui --no-download-sd-model &
+python3.10 /workspace/stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --port 3000 --api --nowebui --no-download-sd-model &
 
 echo "Starting RunPod Handler"
 python3 -u /rp_handler.py
