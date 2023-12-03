7eaa299d80e0641e8234ef5afb5c1e89e2e9c9e7
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-12-02 19:25:00 -0500

    another

--- builder/setup.sh
+++ builder/setup.sh
@@ -16,7 +16,12 @@ apt-get update && apt-get install -y --no-install-recommends \
 # Install Python 3.10
 add-apt-repository ppa:deadsnakes/ppa -y
 apt-get update && apt-get install -y python3.10 python3.10-dev python3.10-distutils
+
+# Update Python 3 alternatives to point to Python 3.10
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+
+# Install pip for Python 3.10
+apt-get install -y python3-pip
 
 # Clean up
 apt-get clean -y && apt-get autoremove -y && rm -rf /var/lib/apt/lists/*
