081a3f9429aff113d7646028186b76e33381b54f
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-12-06 11:34:31 -0500

    changed start.sh

--- src/start.sh
+++ src/start.sh
@@ -8,12 +8,10 @@ source venv/bin/activate
 
 # We start the SD generation webui in the background and redirect the output to a log file
 python3.10 /stable-diffusion-webui/webui.py \ 
-    --skip-python-version-check \
     --skip-torch-cuda-test \
     --nowebui \
     --api \
     --port 3000 \ 
-    --no-download-sd-model \
     > /workspace/webui-api.log 2>&1 &
 
 
