d837604c15c0d9c5634bbec53ef7d8944149e6d4
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-12-06 23:14:52 -0500

    another

--- src/start.sh
+++ src/start.sh
@@ -9,7 +9,7 @@ source venv/bin/activate
 # We start the SD generation webui in the background and redirect the output to a log file
 
 python3.10 /stable-diffusion-webui/webui.py --api --nowebui --port 3000 > /workspace/webui-api.log 2>&1 &
-
+deactivate
 
 echo "Starting RunPod Handler"
 python3 -u /rp_handler.py
