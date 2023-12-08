5a21cd91445baef8084fd2bbdca6cd1f4bb40d2a
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-12-07 21:43:51 -0500

    added more auto-downloads

--- src/setup.sh
+++ src/setup.sh
@@ -37,8 +37,25 @@ sdxl_vae_url="https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.
 upscaler_path="/workspace/models/ESRGAN/4xBox.pth"
 upscaler_url="https://drive.google.com/u/0/uc?id=1KToK9mOz05wgxeMaWj9XFLOE4cnvo40D&export=download"
 
+codeformer_path="/stable-diffusion-webui/models/Codeformer/codeformer-v0.1.0.pth"
+codeformer_url="https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth" 
+
+facelib_path="/stable-diffusion-webui/repositories/CodeFormer/weights/facelib/detection_Resnet50_Final.pth"
+facelib_url="https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth"  
+
+parser_path=" /stable-diffusion-webui/repositories/CodeFormer/weights/facelib/parsing_parsenet.pth"
+parser_url="https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/parsing_parsenet.pth"
+
+sd_path=""/stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors
+sd_url="https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors?download=true"
+
 # Call the function with the path and URL for each file
 # Uncomment the lines below as needed
 download_if_not_exists "$rundiffusionXL_path" "$rundiffusionXL_url"
 download_if_not_exists "$sdxl_vae_path" "$sdxl_vae_url"
 download_if_not_exists "$upscaler_path" "$upscaler_url"
+download_if_not_exists "$codeformer_path" "$codeformer_url"
+download_if_not_exists "$facelib_path" "$facelib_url"
+download_if_not_exists "$parser_path" "$parser_url"
+download_if_not_exists "$sd_path" "$sd_url"
+
