72e6157ea7e1299567400a9a1ed4fba2957d73c0
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-12-15 17:06:25 -0500

    fixed data dir ref

--- src/tasks/train.py
+++ src/tasks/train.py
@@ -21,7 +21,7 @@ def make_train_command(username,  resolution="512,512", model_path=PRETRAINED_MO
         "num_cpu_threads_per_process": 4,
         "script": SCRIPT_PATH,
         "pretrained_model_name_or_path": model_path,
-        "train_data_dir": TRAIN_DATA_DIR_BASE,
+        "train_data_dir": os.path.join(TRAIN_DATA_DIR_BASE, username),
         "resolution": resolution,
         "output_name": username,
         **load_config(CONFIG_PATH)
