eb5565ff8c2f6173fa0b7345c524267380176ab5
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2023-12-31 19:27:46 -0500

    tidying

--- src/tasks/generate/payload/upscaler.py
+++ src/tasks/generate/payload/upscaler.py
@@ -3,22 +3,25 @@ def upscale(json):
         return json
 
     prompt = json.get('prompt', "")
+    height = json.get('height', "1024")
     negative_prompt = json.get('negative_prompt', "")
     upscale_factor = json.get('witit_upscale_by', "1")
     upscale_steps = json.get('witit_upscale_steps', "10")
-    height = json.get('height', "1024")
 
     print(f"Setting upscale to {upscale_factor}")
 
     return {
         **json,
         "enable_hr": True,
+        "hr_upscaler": "4xBox",
         "height": height,
         "hr_negative_prompt": negative_prompt,
+        "hr_second_pass_steps": upscale_steps,
+        "hr_scale": upscale_factor,
         "hr_prompt": prompt,
         "hr_resize_x": 0,
         "hr_resize_y": 0,
-        "hr_scale": upscale_factor,
-        "hr_second_pass_steps": upscale_steps,
-        "hr_upscaler": "4xBox",
+        "seed_enable_extras": True,
+        "seed_resize_from_h": -1,
+        "seed_resize_from_w": -1,
     }
