b6ae19316e968adab5da78d7f1aac73ee0549ead
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2024-01-07 12:16:20 -0500

    rewrote checkpoint and override handling

--- src/tasks/generate/checkpoints.py
+++ src/tasks/generate/checkpoints.py
@@ -9,18 +9,25 @@ from utils.config import load_config
 
 def softlink_checkpoint(checkpoint_path):
     filename = os.path.basename(checkpoint_path)
-    [model_name, ext] = os.path.splitext(filename)
-
     softlink_path = f"/workspace/stable-diffusion-webui/models/Stable-diffusion/{filename}"
 
     if os.path.exists(softlink_path):
-        os.remove(softlink_path)
-        print("Existing softlink with UUID removed")
+        return softlink_path
+    try:
+        subprocess.run(["ln", "-s", checkpoint_path, softlink_path])
+        print(f"Softlinked user checkpoint to {softlink_path}")
+        return softlink_path
+    except Exception as err:
+        print("Error: ", err)
 
-    subprocess.run(["ln", "-s", checkpoint_path, softlink_path])
-    print(f"Softlinked user checkpoint to {softlink_path}")
 
-    return softlink_path, model_name
+def softlink_model_from_name(model_name):
+    full_path = f"/workspace/witit-custom/checkpoints/{model_name}.safetensors"
+    if not os.path.exists(full_path):
+        print(f"Checkpoint {model_name} not found")
+        raise Exception(f"Checkpoint {model_name} not found")
+    softlink_path = softlink_checkpoint(full_path)
+    return softlink_path
 
 
 def refresh_checkpoints():
@@ -49,32 +56,43 @@ def refresh_vae():
         return None
 
 
-def handle_checkpoint(json_data):
-    if 'override_settings' not in json_data or 'sd_model_checkpoint' not in json_data['override_settings']:
-        default_config = load_config("/workspace/config/webui.json")
-        override_settings = default_config.get("override_settings")
-        return {**json_data, "override_settings": override_settings}, None
+def handle_override_settings(json_data):
+    default_config = load_config("/workspace/config/webui.json")
+    override_settings = default_config.get("override_settings")
 
-    checkpoint_path = json_data.get(
-        "override_settings").get("sd_model_checkpoint")
+    if 'override_settings' not in json_data:
+        return {**json_data, "override_settings": override_settings}
+    else:
+        for key, value in override_settings.items():
+            if key not in json_data.get("override_settings"):
+                json_data["override_settings"][key] = value
+        return json_data
 
-    if 'witit-custom' not in checkpoint_path:
-        print("Not a custom checkpoint, skipping softlink")
-        return json_data, checkpoint_path
 
-    softlink_path, model_name = softlink_checkpoint(checkpoint_path)
+def find_model(model_name):
     checkpoints = refresh_checkpoints()
-
     match = None
     for c in checkpoints:
         if c['model_name'] == model_name:
             match = c
-            print("Found matching checkpoint:")
-            print(match)
+            print("Found matching checkpoint:", match)
             break
+    return match
 
-    if match is not None:
-        json_data['override_settings']['sd_model_checkpoint'] = model_name
+
+def handle_checkpoint(json_data):
+    json_data = handle_override_settings(json_data)
+    model_name = json_data.get("override_settings").get("sd_model_checkpoint")
+    match = find_model(model_name)
+
+    if match:
+        return json_data, None
+
+    print("No matching checkpoint found, softlinking user checkpoint")
+    softlink_path = softlink_model_from_name(model_name)
+    match = find_model(model_name)
+
+    if match:
         return json_data, softlink_path
     else:
         print(f"Checkpoint {model_name} not found")
