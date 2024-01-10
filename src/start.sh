1ed1cc768079e6e94ee2b95f45145b92f99b15e0
Jeff DeChambeau <jeff.dechambeau@gmail.com>
2024-01-10 11:42:30 -0500

    added support for WEBUI_API_ARGS env var w/ default

--- src/start.sh
+++ src/start.sh
@@ -8,11 +8,17 @@ mkdir -p /workspace/logs
 # Convert IS_TRAINING_POD_ONLY to lowercase for case-insensitive comparison
 isTrainingPodOnly=$(echo "${IS_TRAINING_POD_ONLY}" | tr '[:upper:]' '[:lower:]')
 
+# Default arguments for launching WebUI API
+defaultWebUIArgs="--api --nowebui --port 3000 --no-half --disable-nan-check --precision full"
+
+# Use the WEBUI_API_ARGS environment variable if set, otherwise use the default
+webUIArgs="${WEBUI_API_ARGS:-$defaultWebUIArgs}"
+
 # Launch WebUI only if isTrainingPodOnly is unset, empty, or set to 'false'
 if [ -z "${isTrainingPodOnly}" ] || [ "${isTrainingPodOnly}" = "false" ]; then
     echo "Starting WebUI API"
     source /venv/stable-diffusion-webui/venv/bin/activate
-    python3.10 /workspace/stable-diffusion-webui/webui.py --api --nowebui --port 3000 --no-half --disable-nan-check --force-fp32 > /workspace/logs/webui-api-${RUNPOD_POD_ID}.log 2>&1 &
+    python3.10 /workspace/stable-diffusion-webui/webui.py $webUIArgs > /workspace/logs/webui-api-${RUNPOD_POD_ID}.log 2>&1 &
     deactivate
 else
     echo "Skipping WebUI API Launch as IS_TRAINING_POD_ONLY is set to true or TRUE"
